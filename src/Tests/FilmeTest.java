12b9e7a7e4f6a53613fc180fe232b9aee361784c
Eduardo Augusto Finger Ballico <ballico591@gmail.com>
2021-12-01 13:14:21 -0300

    Testando a capoeira do zum zum zum

--- src/Tests/FilmeTest.java
+++ src/Tests/FilmeTest.java
@@ -1,20 +1,38 @@
 package Tests;
 
+import AcmeFun.cliente.ClienteEmpresarial;
+import AcmeFun.entretenimento.Filme;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class FilmeTest {
 
+    Filme f;
+
+    @BeforeEach
+    public void start() {
+        f = new Filme("F01", "focco", 2000, 140);
+    }
+
     @Test
     void getTempoDurcao() {
+        assertEquals(f.getTempoDurcao(), 140);
     }
 
     @Test
     void testToString() {
+        assertEquals(f.toString(),"1;F01;focco;2000;140\n");
     }
 
     @Test
     void getTipo() {
+        assertEquals(f.getTipo(), "1");
+    }
+
+    @Test
+    void getPreco() {
+        assertEquals(f.getPreco(), 6);
     }
 }
