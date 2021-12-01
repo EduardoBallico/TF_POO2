cf75092672e28bcaaf4e3e39bba9fd216f97fad5
Eduardo Augusto Finger Ballico <ballico591@gmail.com>
2021-11-30 22:02:01 -0300

    ZUM ZUM ZUM CAPOEIRA FAZ TUM TUM

---
+++ src/Tests/ClienteIndividualTest.java
@@ -0,0 +1,47 @@
+package Tests;
+
+import AcmeFun.cliente.ClienteEmpresarial;
+import AcmeFun.cliente.ClienteIndividual;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ClienteIndividualTest {
+
+    ClienteEmpresarial cE;
+    ClienteIndividual cN;
+    ClienteIndividual cV;
+
+    @BeforeEach
+    public void start(){
+        cE = new ClienteEmpresarial("Focco", "focco@f.com", "123", "12123123123333", "Focco Solucoes");
+        cN = new ClienteIndividual("mts", "mts@email.com","mts", "12312312311", null);
+        cV = new ClienteIndividual("mts", "mts@email.com","mts", "12312312311", cE);
+    }
+
+    @Test
+    void getCpf() {
+        assertEquals(cN.getCpf(),"12312312311");
+    }
+
+    @Test
+    void getEmpresa() {
+        assertEquals(cV.getEmpresa(), cE);
+    }
+
+    //todo
+    @Test
+    void cobrancaMensal() {
+    }
+
+    @Test
+    void testToString() {
+        assertEquals("1;mts;mts@email.com;mts;12312312311", cN.toString());
+    }
+
+    @Test
+    void defineTipo() {
+        assertEquals(cV.defineTipo(), 3);
+    }
+}
