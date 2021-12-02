65d18dc00120a9faa999fc437f7afafab258bb7b
Eduardo Augusto Finger Ballico <ballico591@gmail.com>
2021-12-01 21:25:26 -0300

    mais teste da capoeira q faz tum dum

--- src/Tests/ClienteTests/ListaDeUsuariosTest.java
+++ src/Tests/ClienteTests/ListaDeUsuariosTest.java
@@ -1,13 +1,39 @@
 package Tests.ClienteTests;
 
+import AcmeFun.acesso.Acesso;
+import AcmeFun.acesso.ListaDeAcesso;
+import AcmeFun.cliente.ClienteEmpresarial;
+import AcmeFun.cliente.ClienteIndividual;
+import AcmeFun.cliente.ListaDeUsuarios;
+import AcmeFun.entretenimento.EpisodioSerie;
+import AcmeFun.entretenimento.Filme;
+import AcmeFun.entretenimento.Jogo;
+import AcmeFun.entretenimento.Serie;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class ListaDeUsuariosTest {
 
+    ClienteEmpresarial cE;
+    ClienteIndividual cN;
+    ClienteIndividual cV;
+    ListaDeUsuarios lU;
+
+    @BeforeEach
+    public void start(){
+        cE = new ClienteEmpresarial("Focco", "focco@f.com", "123", "12123123123333", "Focco Solucoes");
+        cN = new ClienteIndividual("mts", "mts@email.com","mts", "12312312311", null);
+        cV = new ClienteIndividual("mts", "mts@email.com","mts", "12312312311", cE);
+   }
+
     @Test
-    void verificaUsuario() {
+    void exiteUsuario() {
+
     }
 
     @Test
@@ -16,6 +42,7 @@ class ListaDeUsuariosTest {
 
     @Test
     void cadastraCliente() {
+        assertTrue(lU.cadastraCliente(cN),false);
     }
 
     @Test
